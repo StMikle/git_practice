62ff5983f8f10e3d28ffe9bfa28bf7dd2607d8b1
StMikle <stehnovich2222@gmail.com>
2021-06-01 14:26:40 +0300

    fead: add new method

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -3,6 +3,10 @@ package mainPackage;
 public class Main {
 
     public static void main(String[] args) {
+        output();
+
+    }
+    public static void output() {
         System.out.println("Hello World");
     }
 }
